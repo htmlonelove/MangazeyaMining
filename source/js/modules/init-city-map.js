c4681b722d2f12cd7a7b88654260648644396acd
pomehy <hlopkov.gleb@mail.ru>
2022-07-21 10:57:56 +0300

    работает над мобильной версией Контакты с картой

--- source/js/modules/init-city-map.js
+++ source/js/modules/init-city-map.js
@@ -1,12 +1,8 @@
-const cityItems = document.querySelectorAll('[data-city]');
+const cityItems = document.querySelectorAll('[data-city-item]');
+const citiTabsControl = document.querySelector('[data-city-controls]');
 
-const initCityMap = () => {
-  if (!cityItems.length) {
-    return;
-  }
-
+const initMapCity = () => {
   for (let i = 0; i < cityItems.length; i++) {
-    const cityContent = cityItems[i].querySelector('[data-city-content]');
     const buttonLocation = cityItems[i].querySelector('[data-city-location]');
 
     buttonLocation.addEventListener('click', (evt) => {
@@ -14,11 +10,58 @@ const initCityMap = () => {
 
       if (cityItems[i].classList.contains('is-hidden')) {
         cityItems[i].classList.remove('is-hidden');
+
+        if (!citiTabsControl) {
+          return;
+        }
+
+        citiTabsControl.classList.remove('is-hidden');
       } else {
         cityItems[i].classList.add('is-hidden');
+
+        if (!citiTabsControl) {
+          return;
+        }
+
+        citiTabsControl.classList.add('is-hidden');
       }
     });
   }
 };
 
-export {initCityMap};
+const initTabsCity = () => {
+  const cityNavButtons = document.querySelectorAll('[data-city-nav]');
+
+  cityNavButtons.forEach((buttonNav) => {
+    buttonNav.addEventListener('click', () => {
+      let array = Array.from(cityNavButtons);
+      let index = array.indexOf(buttonNav);
+
+      array.forEach((button, i) => {
+        if (i === index) {
+          button.classList.add('is-active');
+          for (let j = 0; j < cityItems.length; j++) {
+            if (i === j) {
+              cityItems[j].classList.add('is-active');
+            } else {
+              cityItems[j].classList.remove('is-active');
+            }
+          }
+        } else {
+          button.classList.remove('is-active');
+        }
+      });
+    });
+  });
+};
+
+const initContactsCity = () => {
+  if (!cityItems.length) {
+    return;
+  }
+
+  initMapCity();
+  initTabsCity();
+};
+
+export {initContactsCity};
